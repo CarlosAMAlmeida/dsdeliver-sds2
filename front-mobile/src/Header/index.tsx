8402c9ce8de927bf36e8df7b98b977460f49d479
Carlos Almeida <dpdnegocios@gmail.com>
2021-01-24 16:08:04 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-function Header() {
+function Header() {  
+    const navigation = useNavigation();
+  
+    const handlerOnPress = () => {
+      navigation.navigate('Home');
+    }
+
   return (
-    <View style={styles.container}>
-        <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}>DS Delivery</Text>
-     </View>
+    <TouchableWithoutFeedback onPress={handlerOnPress}>
+      <View style={styles.container}>
+          <Image source={require('../assets/logo.png')} />
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
+     </TouchableWithoutFeedback>
   );
 }
 
